e43e148fc304438aa2781b6c7750e04a0dab6e9e
Lezardstan <gu.valentin@gmail.com>
2025-02-27 20:06:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-Joe From Hawai #406
-His homies are the best, who are they ?
-https://loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo.ng/looooooooooooOooooOoooOoOoOOoooOOoOoooooooooOooooOooooOoooOooOOooooOOOoOooooOooOoOooooOOooooooOOOOoOoooOOoOOOoooooooOOoooOOoOOOoooooooOoooooOoOoooooooooOooooOooooooooOOOOoOooooOOooOoooooooOOoooOooooOoooOooOOOoooooOoOOoooOOooOooooOOoOOOoooOoooOOoooooOoOOoooooooOOoooOooooOoooOOooOOooooOoOoOooooooooooooOOoooooooOooOOOoooooOoOOooooooooOoooOOooooooooOOOOOoooooOOOOooooooooOooooOOOoooooOOOooOoooooOooOooooooooOoooOOoOOooooOOoOooooooOoOoOng
+I like to listen to voice mails, but it's a shame that I never postsecretvoicemail
+My homie is Joe, from Hawaï of course #406
+He just talks a lot, and is really fun !
+One of my favourite is the number 110010110
+His homies are the best, who are they ? Or maybe "Who" is not the correct way to say it, (https://urls.fr/A1Yp08)
+The awnser is:
+-
+and
+-  
+and
+-
+  
+Dang, We need a third one.... 
+I keep my homies close, the first one is even my passcode !
