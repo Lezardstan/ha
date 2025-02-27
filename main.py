9227bd77692a0e7939f116523c7a59918d969559
Lezardstan <gu.valentin@gmail.com>
2025-02-27 20:28:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ My homie is Joe, from Hawaï of course #406
 He just talks a lot, and is really fun !
 One of my favourite is the number 110010110
 His homies are the best, who are they ? Or maybe "Who" is not the correct way to say it, (https://urls.fr/A1Yp08)
-The awnser is:
--
+There's 
 and
--  
+
 and
--
+
   
-Dang, We need a third one.... 
+Dang, where did the third one go ? 
 I keep my homies close, the first one is even my passcode ! 
 (Never judge a book by its cover, a file can be as treacherous)
