2de7527094b590e51eff6e5bda9d2a5eaef6aac5
Lezardstan <gu.valentin@gmail.com>
2025-03-09 20:19:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 I like to listen to voice mails, but it's a shame that I never postsecretvoicemail
 
 Who are the homies ? Or maybe "Who" is not the correct way to say it, (https://urls.fr/A1Yp08)
-There's 
-and
-
-and
-
-  
-Dang, where did the third one go ? 
+There's XXX and XXX and XXXX
+Who's the third ?
 Luckily,the first one is the key to our secrets
 (Never judge a book by its cover)
